335b1befdc95936db7ad2a0fde07e0d4f99fd64a
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2026-09-18 21:35:41 -0700

    Refactor regex compilation to use sre_compile
    Updated regex compilation to use sre_compile instead of re._compile for compatibility.

--- build_support/cpplint.py
+++ build_support/cpplint.py
@@ -56,10 +56,9 @@ import unicodedata
 import xml.etree.ElementTree
 
 try:
-  regex_compile = re._compile.compile
-except AttributeError:
+  import re._compiler as sre_compile
+except ImportError:
   import sre_compile
-  regex_compile = sre_compile.compile
 
 # if empty, use defaults
 _valid_extensions = set([])
@@ -805,7 +804,7 @@ def Match(pattern, s):
   # performance reasons; factoring it out into a separate function turns out
   # to be noticeably expensive.
   if pattern not in _regexp_compile_cache:
-    _regexp_compile_cache[pattern] = regex_compile(pattern)
+    _regexp_compile_cache[pattern] = sre_compile.compile(pattern)
   return _regexp_compile_cache[pattern].match(s)
 
 
@@ -823,14 +822,14 @@ def ReplaceAll(pattern, rep, s):
     string with replacements made (or original string if no replacements)
   """
   if pattern not in _regexp_compile_cache:
-    _regexp_compile_cache[pattern] = regex_compile(pattern)
+    _regexp_compile_cache[pattern] = sre_compile.compile(pattern)
   return _regexp_compile_cache[pattern].sub(rep, s)
 
 
 def Search(pattern, s):
   """Searches the string for the pattern, caching the compiled regexp."""
   if pattern not in _regexp_compile_cache:
-    _regexp_compile_cache[pattern] = regex_compile(pattern)
+    _regexp_compile_cache[pattern] = sre_compile.compile(pattern)
   return _regexp_compile_cache[pattern].search(s)
 
 
